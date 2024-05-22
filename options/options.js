92b09faf8a7c3a9a9d19dcd78cc823af781464f7
Crowdbotics <team@crowdbotics.com>
2024-05-22 20:39:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "build-alex-augenste-48065",
+  url: "https://build-alex-augenste-48065.botics.co",
+  api: "https://build-alex-augenste-48065.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
